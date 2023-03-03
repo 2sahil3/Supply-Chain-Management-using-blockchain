f0ed1077f72f20aaba98d7f76a9a704b01002c10
2sahil3 <sahilmjain03@gmail.com>
2023-03-04 00:32:10 +0530

    digital signature added

--- contracts/supply_chain.sol
+++ contracts/supply_chain.sol
@@ -2,7 +2,7 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
-contract supplychain
+contract Item
 {
     
     string public timestamp;
@@ -10,7 +10,19 @@ contract supplychain
     string public mfgDate;
     string public expiryDate;
     string public batchNo;
-    
+    uint public numberOfItem;
+
+
+    constructor(string memory item_name,string memory time_stamp,string memory mfg_date, string memory expiry_date, string memory batch_no,uint numOfItem )
+    {
+        timestamp = time_stamp;
+        itemName = item_name;
+        mfgDate = mfg_date;
+        expiryDate = expiry_date;
+        batchNo = batch_no;
+        numberOfItem = numOfItem;
+
+    }
 
     function setTimeStamp(string memory time) public
     {
@@ -43,23 +55,42 @@ contract supplychain
     }
    
 
-    function generateQrCode() public 
+    function createHash(string memory item_name,string memory time_stamp,string memory mfg_date, string memory expiry_date, string memory batch_no ) public pure returns(bytes32)// allowed only for manufacturer and distributer
     {
+        bytes32 hash1 = keccak256(abi.encode(item_name,time_stamp,mfg_date, expiry_date, batch_no ));
+        return getEthSignedMessageHash(hash1);
 
     }
 
-    function verifyForDistributor() public
-    {
-
+    function getEthSignedMessageHash(
+        bytes32 _messageHash
+    ) public pure returns (bytes32) {
+        /*
+        Signature is produced by signing a keccak256 hash with the following format:
+        "\x19Ethereum Signed Message\n" + len(msg) + msg
+        */
+        return
+            keccak256(
+                abi.encodePacked("\x19Ethereum Signed Message:\n32", _messageHash)
+            );
     }
-    function verifyForPharmacist() public
-    {
 
-    }
+    // function verifyForDistributor() public
+    // {
 
-    function signDigitally() public{}
+    // }
+    // function verifyForPharmacist() public
+    // {
+
+    // }
+
+    // function signDigitally() public{}
+    
+
+    // bytes32 hash = createHash(itemName, timestamp, mfgDate, expiryDate, batchNo);
 
 }
+
 
 
 
