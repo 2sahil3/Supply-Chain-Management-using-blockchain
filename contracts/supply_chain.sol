5bf2b8771c0e2eddd13f0c09cd89f37fe73b8106
Sahil jain <87411908+2sahil3@users.noreply.github.com>
2023-03-11 17:32:40 +0530

    Add files via upload

--- contracts/supply_chain.sol
+++ contracts/supply_chain.sol
@@ -2,64 +2,80 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
-contract Item
+contract Contract_supplychain
 {
-    
-    string public timestamp;
-    string public itemName;
-    string public mfgDate;
-    string public expiryDate;
-    string public batchNo;
-    uint public numberOfItem;
+    struct Product
+    {
+        string  timestamp;
+        string  itemName;
+        string mfgDate;
+        string expiryDate;
+        string batchNo;
+        uint  numberOfItem;
+        uint productId;
+    }
+    uint public runningProductId = 0;
+    // mapping(uint=>Product) map ;
+    Product[] public products;
 
 
-    constructor(string memory item_name,string memory time_stamp,string memory mfg_date, string memory expiry_date, string memory batch_no,uint numOfItem )
-    {
-        timestamp = time_stamp;
-        itemName = item_name;
-        mfgDate = mfg_date;
-        expiryDate = expiry_date;
-        batchNo = batch_no;
-        numberOfItem = numOfItem;
 
-    }
-
-    function setTimeStamp(string memory time) public
+    function createProduct(string memory item_name,string memory time_stamp,string memory mfg_date, string memory expiry_date, string memory batch_no,uint numOfItem ) public returns(uint)
     {
-        timestamp  = time;
-    }
-    function setMfgDate(string memory date) public
-    {
-        mfgDate = date;
-    }
-    function setExpiryDate(string memory date) public
-    {
-        expiryDate  = date;
+        Product memory p1;
+        p1.timestamp = time_stamp;
+        p1.itemName = item_name;
+        p1.mfgDate = mfg_date;
+        p1.expiryDate = expiry_date;
+        p1.batchNo = batch_no;
+        p1.numberOfItem = numOfItem;
+        p1.productId = products.length;
+        products.push(p1);
+        return p1.productId;
+        // map[runningProductId+1] = p1;
     }
 
-    function getTimeStamp() public view returns(string memory)
+    function getProductDetails(uint productId) public view returns( Product memory) 
     {
-        return timestamp;
+        return products[productId];
+        // return map[productId];
     }
-    function getName() public view returns(string memory)
-    {
-        return itemName;
-    }
-    function getMfgDate() public view returns(string memory)
-    {
-        return mfgDate;
-    }
-    function getExpiryDate() public view returns(string memory)
-    {
-        return expiryDate;
-    }
+
+    // function setTimeStamp(string memory time) public
+    // {
+    //     timestamp  = time;
+    // }
+    // function setMfgDate(string memory date) public
+    // {
+    //     mfgDate = date;
+    // }
+    // function setExpiryDate(string memory date) public
+    // {
+    //     expiryDate  = date;
+    // }
+
+    // function getTimeStamp() public view returns(string memory)
+    // {
+    //     return timestamp;
+    // }
+    // function getName() public view returns(string memory)
+    // {
+    //     return itemName;
+    // }
+    // function getMfgDate() public view returns(string memory)
+    // {
+    //     return mfgDate;
+    // }
+    // function getExpiryDate() public view returns(string memory)
+    // {
+    //     return expiryDate;
+    // }
    
 
     function createHash(string memory item_name,string memory time_stamp,string memory mfg_date, string memory expiry_date, string memory batch_no ) public pure returns(bytes32)// allowed only for manufacturer and distributer
     {
         bytes32 hash1 = keccak256(abi.encode(item_name,time_stamp,mfg_date, expiry_date, batch_no ));
         return getEthSignedMessageHash(hash1);
-
     }
 
     function getEthSignedMessageHash(
