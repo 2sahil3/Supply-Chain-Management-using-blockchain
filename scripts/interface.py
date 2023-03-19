072c91aec4f2b040f1dfa63b3e674bec0ea4b29f
Sahil jain <87411908+2sahil3@users.noreply.github.com>
2023-03-19 22:09:49 +0530

    Add files via upload

--- scripts/interface.py
+++ scripts/interface.py
@@ -1,70 +1,21 @@
-from datetime import datetime
-import json
 from web3 import Web3
-from datetime import datetime
-from hexbytes import HexBytes
 from abi import ABI
-from web3.middleware import geth_poa_middleware
 import time
 
-ganach_url = "http://127.0.0.1:7546"
+ganach_url = "http://127.0.0.1:7545"
 web3 = Web3(Web3.HTTPProvider(ganach_url))
-web3.middleware_onion.inject(geth_poa_middleware, layer=0)
-#web3 = Web3(Web3.EthereumTesterProvider)
 web3.eth.defaultAccount = web3.eth.accounts[0]
-#address = web3.toChecksumAddress("0xd9145CCE52D386f254917e481eB44e9943F39138")
 contract = web3.eth.contract(
-    address="0xFD9F9d320819ECEd465c6C1497Bfa5d564136b6D", abi=ABI)
-# contract = web3.eth.contract(abi = abi, bytecode = bytecode )
-# print(web3.eth.get_block('latest'))
-
-# print(msg_hash)
-# print(type(msg_hash))
-
-# web3.utils.padLeft(web3.utils.hexToBytes(msg_hash), 32)
-# msg_hashbytes = HexBytes(msg_hash)
-# print(msg_hash.hex())
-
+    address="0x2A45890fc0FadF76b58f77Cc4F3f98598A959424", abi=ABI)
 
 signer_account = web3.eth.accounts[0]
-# signer_private = '0x1f03802300a542709b103fc04b631f20e5ae72792b4aa96a9990b4ee39d09d74'
-nonce = web3.eth.get_transaction_count(signer_account)
+
 timeStamp = str(time.time())
 
-print(nonce)
-# tx = {
-#     'nonce': nonce+1,
-#     'to': web3.eth.accounts[1],
-#     'value': web3.toWei(1, 'ether'),
-#     'gas': 2000000,
-#     'gasPrice': web3.toWei('50', 'gwei')
-# }
-# msg_hash = contract.functions.getMessageHash(web3.eth.accounts[1],web3.toWei(1,'ether'),"first transaction",nonce).transact()
-
-# get transaction hash
-# print(web3.toHex(tx_hash))
 
 
-# verifyer_acc = web3.eth.accounts[1]
-# print(web3.eth.accounts[0].verify(signed_tx, msg_hash))
-
-
-def settime():
-    now = "prateek"
-#  print(now)
-    tx_hash = contract.functions.setTimeStamp(now).transact()
-    return tx_hash
-
-
-def gettime():
-    time = contract.functions.getTimeStamp().call()
-    return time
-
-
-def create_product(
-    timeStamp, itemName, mfgDate, expiryDate, batchNo, numberUnits
-):
-
+def create_product(timeStamp, itemName, mfgDate, expiryDate, batchNo, numberUnits):
+    nonce = web3.eth.get_transaction_count(signer_account)
     tx = contract.functions.createProduct(
         itemName,
         timeStamp,
@@ -72,26 +23,25 @@ def create_product(
         expiryDate,
         batchNo,
         numberUnits
-    ).buildTransaction({'from': signer_account})
+    ).buildTransaction({'from': signer_account, 'nonce': nonce})
 
-    signed_tx = web3.eth.account.sign_Transaction(
-        tx, '0x68e6a2d8315b68a792e06b8995e2d6846cdb67c6b7781445e6d341d63d408fd6')
+    signed_tx = web3.eth.account.signTransaction(
+        tx, '0x6727e3c0a0187676c78ff449e9ce460ae1b3479b10e71e59a94414865c8033bc')
     hash_txn = web3.eth.send_raw_transaction(signed_tx.rawTransaction)
-    print(hash_txn)
 
 
-create_product(
-    timeStamp=timeStamp,
-    itemName="item",
-    mfgDate=timeStamp,
-    expiryDate=timeStamp,
-    batchNo="256",
-    numberUnits=123
-)
+def addState(product_id,time_stamp,loc):
+    nonce = web3.eth.get_transaction_count(signer_account)
+    tx = contract.functions.addState(product_id,time_stamp,loc).buildTransaction({'from': signer_account, 'nonce': nonce})
+    signed_tx = web3.eth.account.signTransaction(tx, '0x6727e3c0a0187676c78ff449e9ce460ae1b3479b10e71e59a94414865c8033bc')
+    hash_txn = web3.eth.send_raw_transaction(signed_tx.rawTransaction)
 
 
-def last():
-    return contract.functions.getLastIndex().call()
+def getStates(product_id):
+    states = contract.functions.getState(product_id).call()
+
+def lastProductId():
+    return contract.functions.getLastIndex().call() -1
 
 
 def get_product(productId):
@@ -99,10 +49,5 @@ def get_product(productId):
     return productData
 
 
-# hash = settime()
-# time = gettime()
-# tx_receipt = web3.eth.wait_for_transaction_receipt(hash)
-# print(tx_receipt.contractAddress)
-# time = gettime()
-# print(hash)
-# print(time)
+
+
--- scripts/main.py
+++ scripts/main.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from flask import render_template, request
 import time
-from interface import create_product, get_product, last
+from interface import *
+
 app = Flask(__name__)
 
 
@@ -9,22 +10,28 @@ app = Flask(__name__)
 def manufacturer_page():
     if request.method == "POST":
         timeStamp = str(time.time())
-        # itemName = request.form["item-name"]
-        # mfgDate = request.form["manufacturer-date"]
-        # expiryDate = request.form["expiry-date"]
-        # batchNo = request.form["batch-number"]
-        # numberUnits = int(request.form["number-units"])
-        # print(ID)
-        # ID += 1
-        print(create_product(
+        itemName = request.form["item-name"]
+        mfgDate = request.form["manufacturer-date"]
+        expiryDate = request.form["expiry-date"]
+        batchNo = request.form["batch-number"]
+        numberUnits = int(request.form["number-units"])
+
+        create_product(
             timeStamp=timeStamp,
-            itemName="item",
-            mfgDate=timeStamp,
-            expiryDate=timeStamp,
-            batchNo="256",
-            numberUnits=123
-        ))
-        print(last())
+            itemName=itemName,
+            mfgDate=mfgDate,
+            expiryDate=expiryDate,
+            batchNo=batchNo,
+            numberUnits=numberUnits
+        )
+
+        id = lastProductId()
+
+        addState(id,"123456","mumbai")
+        addState(id,"123456","dubai")
+        print(getStates(id))
+
+
 
         return render_template('manufacturer.html', submitted=True)
 
