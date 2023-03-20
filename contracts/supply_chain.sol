570d5edbe8a021f2b3132d53e930dfdfd44b90d5
Sahil jain <87411908+2sahil3@users.noreply.github.com>
2023-03-20 15:40:34 +0530

    Add files via upload
    change in Products

--- contracts/supply_chain.sol
+++ contracts/supply_chain.sol
@@ -4,83 +4,91 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract Contract_supplychain
 {
-
-    struct State
-    {
-        string timestamp;
-        string location;
-    }
-
     struct Product
     {
-        string  itemName;
-        // string  timestamp;
+        string itemName;
         string mfgDate;
         string expiryDate;
         string batchNo;
-        uint  numberOfItem;
-        uint productId;
-        State[] history;
+        string current_history;
+        uint ID;
+        uint numberOfItem;
+        // int par; 
     }
 
-    // mapping(address => role)
-
-
+      
     uint public runningProductId=0;
+    mapping(address=>Product[]) findMap;
     Product[] public products;
-    function createProduct(string memory item_name,string memory mfg_date, string memory expiry_date, string memory batch_no,uint numOfItem ) public
+    function createProduct(string memory item_name,string memory mfg_date, string memory expiry_date, string memory batch_no,uint numOfItem, string memory history,int parent,address user_address ) public
     {
+        if(parent==-1){
         Product memory p1;
-        // p1.timestamp = time_stamp;
         p1.itemName = item_name;
         p1.mfgDate = mfg_date;
         p1.expiryDate = expiry_date;
         p1.batchNo = batch_no;
+        p1.current_history=history;
         p1.numberOfItem = numOfItem;
-        p1.productId = runningProductId;
+        p1.ID = runningProductId;
         runningProductId++;
-        // require(msg.sender == manufacturer);
-        // addstate() call this function to add the state
+        // p1.par=parent;
+        findMap[user_address].push(p1);
+        products.push(p1);
+    }
+        else{
+            require(products[uint(parent)].numberOfItem>=numOfItem);
+            Product memory p1;
+            p1.itemName = item_name;
+            p1.mfgDate = mfg_date;
+            p1.expiryDate = expiry_date;
+            p1.batchNo = batch_no;
+            p1.current_history=history;
+            p1.numberOfItem = numOfItem;
+            p1.ID = runningProductId;
+            runningProductId++;
+            // p1.par=parent;
+            products.push(p1);
+            findMap[user_address].push(p1);
+            products[uint(parent)].numberOfItem-=numOfItem;
+        }
+        
     }
 
-    function addState(uint product_id, string memory timestamp, string memory location) public 
-    {
-        State memory s1;
-        s1.timestamp = timestamp;
-        s1.location = location;
-        products[product_id].history.push(s1);
+    function findProduct(address person) public view returns (Product[] memory){
+        if(findMap[person].length!=0)
+        {
+            return findMap[person];
+        }
+        Product[] memory arr;
+        return arr;
     }
+
 
     function getLastIndex() public view returns(uint)
     {
         return runningProductId;
     }
-
-    function getLastProduct() public view returns(Product memory){
-        Product memory xero;
-        xero.productId=1;
-        if(products.length==0)return xero;
-        return products[products.length-1];
-    }
-
     function getProductDetails(uint productId) public view returns( Product memory) 
     {
         return products[productId];
     }
-    function createHash(string memory item_name,string memory time_stamp,string memory mfg_date, string memory expiry_date, string memory batch_no ) public pure returns(bytes32)// allowed only for manufacturer and distributer
-    {
-        bytes32 hash1 = keccak256(abi.encode(item_name,time_stamp,mfg_date, expiry_date, batch_no ));
-        return getEthSignedMessageHash(hash1);
-    }
 
-    function getEthSignedMessageHash(
-        bytes32 _messageHash
-    ) public pure returns (bytes32) {
+    
+    // function createHash(string memory item_name,string memory time_stamp,string memory mfg_date, string memory expiry_date, string memory batch_no ) public pure returns(bytes32)// allowed only for manufacturer and distributer
+    // {
+    //     bytes32 hash1 = keccak256(abi.encode(item_name,time_stamp,mfg_date, expiry_date, batch_no ));
+    //     return getEthSignedMessageHash(hash1);
+    // }
+
+    // function getEthSignedMessageHash(
+    //     bytes32 _messageHash
+    // ) public pure returns (bytes32) {
         
-        return
-            keccak256(
-                abi.encodePacked("\x19Ethereum Signed Message:\n32", _messageHash)
-            );
-    }
+    //     return
+    //         keccak256(
+    //             abi.encodePacked("\x19Ethereum Signed Message:\n32", _messageHash)
+    //         );
+    // }
 
 }
