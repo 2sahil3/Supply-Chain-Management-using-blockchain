40fd3083356a291a08a055253c580eb0096442b6
The-Sanskar-Jain0901 <77044966+The-Sanskar-Jain0901@users.noreply.github.com>
2023-04-12 11:19:08 +0530

    Add files via upload

---
+++ qr.py
@@ -0,0 +1,26 @@
+import io
+import qrcode
+from flask import Flask, render_template, Response
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    # Generate the QR code
+    qr = qrcode.QRCode(version=1, box_size=10, border=5)
+    qr.add_data('0')
+    qr.make(fit=True)
+    img = qr.make_image(fill_color='black', back_color='white')
+
+    # Convert the image to bytes
+    img_bytes = io.BytesIO()
+    img.save(img_bytes, format='PNG')
+    img_bytes.seek(0)
+
+    # Return the image as a response
+    return Response(img_bytes, mimetype='image/png')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
