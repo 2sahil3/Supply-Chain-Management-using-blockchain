1a9d38a812fb19ebb73f2670ca5f9ae1c62278b6
The-Sanskar-Jain0901 <77044966+The-Sanskar-Jain0901@users.noreply.github.com>
2023-04-14 23:43:35 +0530

    Update supply_chain.sol

--- contracts/supply_chain.sol
+++ contracts/supply_chain.sol
@@ -6,57 +6,72 @@ contract Contract_supplychain
 {
     struct Product
     {
-        string  itemName;
+        string itemName;
         string mfgDate;
         string expiryDate;
         string batchNo;
         string current_history;
         uint ID;
         uint numberOfItem;
-        address addr;
-        int par;
     }
-    
+ 
     uint public runningProductId=0;
-    mapping(address=>uint[]) findMap;
+    mapping(address=>Product[]) findMap;
     Product[] public products;
-    function createProduct(string memory item_name,string memory mfg_date, string memory expiry_date, string memory batch_no,uint numOfItem, string memory ch,int par,address aadress ) public
-    {   if(par==-1){
-        Product memory p1;
-        p1.itemName = item_name;
-        p1.mfgDate = mfg_date;
-        p1.expiryDate = expiry_date;
-        p1.batchNo = batch_no;
-        p1.current_history=ch;
-        p1.numberOfItem = numOfItem;
-        p1.ID = runningProductId;
-        runningProductId++;
-        p1.par=par+1;
-        products.push(p1);
-
-    }
-        else{
-            require(products[uint(par)].numberOfItem>=numOfItem);
+    function createProduct(string memory item_name,string memory mfg_date, string memory expiry_date, string memory batch_no,uint numOfItem, string memory history,int parent,address user_address ) public returns(uint)
+    {
+        if(parent==-1)
+        {
             Product memory p1;
             p1.itemName = item_name;
             p1.mfgDate = mfg_date;
             p1.expiryDate = expiry_date;
             p1.batchNo = batch_no;
-            p1.current_history=ch;
+            p1.current_history=history;
             p1.numberOfItem = numOfItem;
             p1.ID = runningProductId;
             runningProductId++;
-            p1.par=par+1;
+            findMap[user_address].push(p1);
             products.push(p1);
-            products[uint(par)].numberOfItem-=numOfItem;
+            return p1.ID;
+       
         }
+        else{
+            for(int i=0;uint(i)<findMap[user_address].length;i++){
+                if(findMap[user_address][uint(i)].ID==uint(parent))
+                {   parent=i;
+                    break;
+                }
+            }
+            require(findMap[user_address][uint(parent)].numberOfItem>=numOfItem);
+            Product memory p1;  
+            p1.itemName = item_name;
+            p1.mfgDate = mfg_date;
+            p1.expiryDate = expiry_date;
+            p1.batchNo = batch_no;
+            p1.current_history=history;
+            p1.numberOfItem = numOfItem;
+            p1.ID = runningProductId;
+            runningProductId++;
+            products.push(p1);
+            findMap[user_address].push(p1);
+            findMap[user_address][uint(parent)].numberOfItem-=numOfItem;
+            return p1.ID;
+        }
+
         
     }
 
-     function findProd(address person) public returns (Product[] memory){
-         Product[] memory arr;
-         
-     }
+    function findProduct(address person) public view returns (Product[] memory){
+        if(findMap[person].length!=0)
+        {
+            return findMap[person];
+        }
+        Product[] memory arr;
+        return arr;
+    }
+
+
     function getLastIndex() public view returns(uint)
     {
         return runningProductId;
@@ -65,20 +80,12 @@ contract Contract_supplychain
     {
         return products[productId];
     }
-    function createHash(string memory item_name,string memory time_stamp,string memory mfg_date, string memory expiry_date, string memory batch_no ) public pure returns(bytes32)// allowed only for manufacturer and distributer
-    {
-        bytes32 hash1 = keccak256(abi.encode(item_name,time_stamp,mfg_date, expiry_date, batch_no ));
-        return getEthSignedMessageHash(hash1);
-    }
 
-    function getEthSignedMessageHash(
-        bytes32 _messageHash
-    ) public pure returns (bytes32) {
-        
-        return
-            keccak256(
-                abi.encodePacked("\x19Ethereum Signed Message:\n32", _messageHash)
-            );
+    function getLastProductId(address addr) public view returns(uint)
+    {
+        Product[] memory p =  findMap[addr];
+        uint id = findMap[addr][p.length-1].ID;
+        return id;
     }
 
 }
